1600b794f525e309695c92a63aa724a7c7af963b
Rasleson02 <Rasleson02>
2014-04-23 17:52:06 +0000

    crated a grocery list program that uses a hash to store items and values

---
+++ grocery_list.rb
@@ -0,0 +1,16 @@
+grocery_list = Hash.new
+puts"what do you want to get at the store ?"
+item =gets.chomp
+while item !="done"
+  if grocery_list.has_key?(item)
+  grocery_list[item] += 1
+  else
+  grocery_list[item] = 1
+  end  
+  puts"what do you want to get at the store ? Type the item or type done"
+item =gets.chomp
+  
+end
+grocery_list.each do |item, number|
+  puts"#{item}~#{number}"
+end
